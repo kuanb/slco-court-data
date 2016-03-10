bcb6e1d4698a3396a1b9175118befa5f3a086a7e
Kuan Butts <kuanbutts@yahoo.com>
2016-03-09 16:33:23 -0800

    loading in w csv parseer

--- server/app.js
+++ server/app.js
@@ -1,10 +1,46 @@
+// creds, settings
+var creds = require("../credentials.js");
+
 // app initialization
 var express = require("express");
 var app = express();
 
+// libraries
+var JSFtp = require("jsftp");
+var AWS = require("aws-sdk");
+var csv = require("csv");
 
-var port = 4000;
-app.listen(port, function () { console.log("Listening on port", port); });
+
+var ftp = new JSFtp(creds.ftp);
+console.log("Loaded");
+
+var str = "";
+ftp.get("./incoming/vine_case.ul", function (err, socket) {
+	if (err) {
+		return false;
+	} else {
+		socket.on("data", function (d) { str += d.toString(); })
+		socket.on("close", function (err) {
+			if (err) {
+				console.error("Error retrieving the file: ", err);
+			} else {
+				csv.parse(str, {delimiter: "\t"}, function (err, output) {
+					if (err) {
+						return false
+					} else {
+						console.log("DONE", output[0])
+					}
+				});
+			}
+		});
+		socket.resume();
+	}
+});
+
+
+
+// var port = 4040;
+// app.listen(port, function () { console.log("Listening on port", port); });
 
 
 
