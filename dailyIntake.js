9eebe64d5c3cc5d25d4304ef812c09f7c07b2459
Kuan Butts <kuanbutts@yahoo.com>
2016-03-11 11:07:09 -0800

    rm sensitive data from api return

--- dailyIntake.js
+++ dailyIntake.js
@@ -50,21 +50,21 @@ function parseAndInput (str, tablename) {
 
 				if (tablename == "case_record") {
 					e = {
-						location_code: o[1] == false ? null : o[1],
-						court_type: o[2] == false ? null : o[2],
-						party_num: o[3] == false ? null : o[3],
-						case_num: o[4] == false ? null : o[4],
-						defendant_first: o[5] == false ? null : o[5],
-						defendant_last: o[6] == false ? null : o[6],
-						disposition_date: o[7] == false ? null : o[7],
-						disposition_code: o[8] == false ? null : o[8],
-						bail_amount: o[9] == false ? null : o[9],
-						ssn: o[10] == false ? null : o[10],
-						birth_date: o[11] == false ? null : o[11],
-						race_code: o[12] == false ? null : o[12],
-						gender: o[13] == false ? null : o[13],
-						disposition_desc: o[14] == false ? null : o[14],
-						booking_num: o[15] == false ? null : o[15]
+						location_code:    !o[1]  ? null : o[1],
+						court_type:       !o[2]  ? null : o[2],
+						party_num:        !o[3]  ? null : o[3],
+						case_num:         !o[4]  ? null : o[4],
+						defendant_first:  !o[5]  ? null : o[5],
+						defendant_last:   !o[6]  ? null : o[6],
+						disposition_date: !o[7]  ? null : o[7],
+						disposition_code: !o[8]  ? null : o[8],
+						bail_amount:      !o[9]  ? null : o[9],
+						ssn:              !o[10] ? null : o[10],
+						birth_date:       !o[11] ? null : o[11],
+						race_code:        !o[12] ? null : o[12],
+						gender:           !o[13] ? null : o[13],
+						disposition_desc: !o[14] ? null : o[14],
+						booking_num:      !o[15] ? null : o[15]
 					};
 
 					// clean up certain types
@@ -83,20 +83,20 @@ function parseAndInput (str, tablename) {
 
 				} else if (tablename == "court_event") {
 					e = {
-						location_code: o[1] == false ? null : o[1],
-						court_type: o[2] == false ? null : o[2],
-						party_num: o[3] == false ? null : o[3],
-						case_num: o[4] == false ? null : o[4],
-						defendant_first: o[5] == false ? null : o[5],
-						defendant_last: o[6] == false ? null : o[6],
-						appear_date: o[7] == false ? null : o[7],
-						appear_time: o[8] == false ? null : o[8],
-						hearing_code: o[9] == false ? null : o[9],
-						court_room: o[10] == false ? null : o[10],
-						court_int_case_num: o[11] == false ? null : o[11],
-						created: o[12] == false ? null : o[12],
-						cancelled: o[13] == false ? null : o[13],
-						cancel_reason: o[14] == false ? null : o[14]
+						location_code:      !o[1]  ? null : o[1],
+						court_type:         !o[2]  ? null : o[2],
+						party_num:          !o[3]  ? null : o[3],
+						case_num:           !o[4]  ? null : o[4],
+						defendant_first:    !o[5]  ? null : o[5],
+						defendant_last:     !o[6]  ? null : o[6],
+						appear_date:        !o[7]  ? null : o[7],
+						appear_time:        !o[8]  ? null : o[8],
+						hearing_code:       !o[9]  ? null : o[9],
+						court_room:         !o[10] ? null : o[10],
+						court_int_case_num: !o[11] ? null : o[11],
+						created:            !o[12] ? null : o[12],
+						cancelled:          !o[13] ? null : o[13],
+						cancel_reason:      !o[14] ? null : o[14]
 					};
 
 					// clean up certain types
@@ -128,21 +128,21 @@ function parseAndInput (str, tablename) {
 
 				} else if ("charge_record") {
 					e = {
-						location_code: o[1] == false ? null : o[1],
-						court_type: o[2] == false ? null : o[2],
-						party_num: o[3] == false ? null : o[3],
-						case_num: o[4] == false ? null : o[4],
-						violation_code: o[5] == false ? null : o[5],
-						severity: o[6] == false ? null : o[6],
-						charge_sequence: o[7] == false ? null : o[7],
-						amount_due: o[8] == false ? null : o[8],
-						judgement_code: o[9] == false ? null : o[9],
-						case_type: o[10] == false ? null : o[10],
-						offense_desc: o[11] == false ? null : o[11],
-						violation_date: o[12] == false ? null : o[12],
-						arrest_date: o[13] == false ? null : o[13],
-						judgement_date: o[14] == false ? null : o[14],
-						judgement_desc: o[15] == false ? null : o[15]
+						location_code:   !o[1]  ? null : o[1],
+						court_type:      !o[2]  ? null : o[2],
+						party_num:       !o[3]  ? null : o[3],
+						case_num:        !o[4]  ? null : o[4],
+						violation_code:  !o[5]  ? null : o[5],
+						severity:        !o[6]  ? null : o[6],
+						charge_sequence: !o[7]  ? null : o[7],
+						amount_due:      !o[8]  ? null : o[8],
+						judgement_code:  !o[9]  ? null : o[9],
+						case_type:       !o[10] ? null : o[10],
+						offense_desc:    !o[11] ? null : o[11],
+						violation_date:  !o[12] ? null : o[12],
+						arrest_date:     !o[13] ? null : o[13],
+						judgement_date:  !o[14] ? null : o[14],
+						judgement_desc:  !o[15] ? null : o[15]
 					};
 
 					// clean up certain types
--- server/app.js
+++ server/app.js
@@ -24,29 +24,72 @@ app.get("/api/v1", function (req, res) {
 		received_at: new Date(Date.now()).toUTCString()
   }
 
-	var processed_at = new Date(Date.now()).toUTCString();
 
-	var first = "%", 
-			last = "%", 
-			dob = "%";
-	// if (req.params.hasOwnProperty("first_name"))
-	
-	db("case_record").join()
-	.join("court_event", function () {
-		this.on("case_record.party_num", "=", "court_event.party_num")
-		.andOn("case_record.case_num", "=", "court_event.case_num")
-		.andOn("case_record.case_num", "=", "court_event.case_num")
-	})
-	.where("defendant_first", "like", first).limit(5).then(function (defendants) {
+	var q = db("case_record")
+					.innerJoin("court_event", function () {
+						this.on("case_record.party_num", "=", "court_event.party_num")
+						.andOn("case_record.case_num", "=", "court_event.case_num")
+					}).limit(100);
 
-		res.send({
+	if (req.query.hasOwnProperty("first_name")) {
+		var first = req.query.first_name.toUpperCase();
+		q.where("case_record.defendant_first", "like", "%" + first + "%");
+	}
+	if (req.query.hasOwnProperty("last_name")) {
+		var last = req.query.last_name.toUpperCase();
+		q.where("case_record.defendant_last", "like", "%" + last + "%");
+	}
+	if (req.query.hasOwnProperty("dob")) {
+		var dob = req.query.dob.toUpperCase();
+		q.where("case_record.birth_date", "=", dob);
+	}
+
+	q.then(function (defendants) {
+
+		defendants = defendants.map(function (d) {
+			var revised = {
+				location_code: d.location_code,
+				court_type: d.court_type,
+				party_num: d.party_num,
+				case_num: d.case_num,
+				defendant_first: d.defendant_first,
+				defendant_last: d.defendant_last,
+				disposition_date: d.disposition_date,
+				disposition_code: d.disposition_code,
+				bail_amount: d.bail_amount,
+				birth_date: d.birth_date == null ? null : new Date(d.birth_date).toDateString(),
+				race_code: d.race_code,
+				gender: d.gender,
+				disposition_desc: d.disposition_desc,
+				booking_num: d.booking_num,
+				appear_date: d.appear_date == null ? null : new Date(d.appear_date).toDateString(),
+				appear_time: d.appear_time,
+				hearing_code: d.hearing_code,
+				court_room: d.court_room,
+				court_int_case_num: d.court_int_case_num,
+				created: d.created == null ? null : new Date(d.created).toDateString(),
+				cancelled: d.cancelled == null ? null : new Date(d.cancelled).toDateString(),
+				cancel_reason: d.cancel_reason
+			};
+			return revised;
+		});
+
+		res.json({
 		  request: request,
-		  processed_at: processed_at,
+		  processed_at: new Date(Date.now()).toUTCString(),
 		  errors: [],
 		  results_count: defendants.length,
 		  results: defendants
 		});
 
+	}).catch(function (error) {
+		res.json({
+		  request: request,
+		  processed_at: new Date(Date.now()).toUTCString(),
+		  errors: [error],
+		  results_count: 0,
+		  results: []
+		})
 	});
 
 });
